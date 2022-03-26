90be8aa69281252fa3e8eca6628ef0c44b3b8b18
reymund <reymund.lapera@gmail.com>
2022-03-26 23:10:15 +0100

    working 3 test cases

--- src/test/java/com/planitestexam/uitest/UITest_Runner.java
+++ src/test/java/com/planitestexam/uitest/UITest_Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/com/planitestexam/uitest",
+        features = "src/test/resources/features",
         tags = "@all",
         glue = {"com.planitestexam.uitest","com.planitestexam.bdd.hooks"},
         plugin = { "pretty", "html:target/cucumber-reports/cucumber.html",
